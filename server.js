2ebcd067fc97034a4dd184840454e6fff43681dc
AshokPatidar <ashokpatidardev@gmail.com>
2021-08-15 22:29:40 +0530

    remove client build from backend

--- server.js
+++ server.js
@@ -6,47 +6,48 @@ const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 const path = require('path')
 
-
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
-app.use(fileUpload({
-    useTempFiles: true
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+)
 
 // Routes
 app.use('/user', require('./src/routes/userRouter'))
 app.use('/api', require('./src/routes/upload'))
 
-
 // Connect to mongodb
 const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
+mongoose.connect(
+  URI,
+  {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
-    if(err) throw err;
-    console.log("Connected to mongodb")
-})
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err
+    console.log('Connected to mongodb')
+  }
+)
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('backend is running')
+  })
 }
-else {
-    app.get("/", (req,res)=> {
-        res.send("backend is running");
-    });
-}
 
-
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT)
+  console.log('Server is running on port', PORT)
 })
